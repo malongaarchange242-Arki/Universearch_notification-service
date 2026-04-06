a7a9f857118c0dd348d3b2b26a4d86d4ee4d43b9
Archange Malonga <malongaarchange242@gmail.com>
2026-04-06 23:59:17 +0100

    Mise à jour

--- lib/notification_service.ex
+++ lib/notification_service.ex
@@ -3,14 +3,31 @@ defmodule NotificationService.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      NotificationService.Repo,
-      NotificationService.Push.AccessTokenCache,
-      {Oban, Application.fetch_env!(:notification_service, Oban)},
-      NotificationServiceWeb.Endpoint
-    ]
+    children =
+      [
+        NotificationService.Repo,
+        NotificationService.Push.AccessTokenCache,
+        NotificationServiceWeb.Endpoint
+      ]
+      |> maybe_add_oban()
 
     opts = [strategy: :one_for_one, name: NotificationService.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp maybe_add_oban(children) do
+    if oban_enabled?() do
+      List.insert_at(
+        children,
+        2,
+        {Oban, Application.fetch_env!(:notification_service, Oban)}
+      )
+    else
+      children
+    end
+  end
+
+  defp oban_enabled? do
+    System.get_env("OBAN_ENABLED", "true") != "false"
   end
 end
